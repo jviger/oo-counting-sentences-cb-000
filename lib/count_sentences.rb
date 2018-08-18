60cf66cbd9d22971a05916bea265fecc08ced83c
Jason Viger <jason.viger@gmail.com>
2018-08-18 10:06:02 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,17 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      TRUE
+    else 
+      FALSE
   end
 
   def question?
-
+if self.end_with?(".")
+      TRUE
+    else 
+      FALSE
   end
 
   def exclamation?
